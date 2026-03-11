1d26a08f460d5768511413aaa8db87d8073bafbb
Komal <komalbhu007@gmail.com>
2026-03-11 17:14:58 +0530

    Day 3: Professional disk_alert.sh with header and variables

---
+++ scripts/disk_alert.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ # ===============================================================
+ #  Script: disk_alert.sh
+ #  Author: Komal
+ #  Version: 1.0
+ # Description: Monitors disk usage
+ #  ===============================================================
+
+ #Configuration
+ THRESHOLD=80
+ PARTITION="/"
+
+ check_disk(){
+  USED=$(df -h $1 | awk 'NR==2{printf "%.0f", $5}'| tr -d '%')
+  if [ $USED -gt $THRESHOLD ]; then
+	  echo "WARNING: $1 is Critical: ${USED}%"
+  else
+	  echo "OK: $1 is Healthy: ${USED}%"
+  fi
+
+ }
+ check_disk $PARTITION
+ check_disk /boot/efi
+
+echo ""
+# =================================================================
+
+# =================================================================
