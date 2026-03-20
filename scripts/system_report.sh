0b59da4ce2c8cbb4df4976833993ffea8e944c81
Komal <komalbhu007@gmail.com>
2026-03-20 11:47:39 +0530

    feature: add logging to system_report.sh

--- scripts/system_report.sh
+++ scripts/system_report.sh
@@ -8,30 +8,35 @@
 
 set -e
 set -u
+LOG_FILE=~/devops-journey/logs/system_report.log
 
-echo "================================"
-echo "DISK USAGE"
-echo "================================"
+log(){
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a $LOG_FILE
+}
+log "================================"
+log "DISK USAGE"
+log "================================"
 
  PARTITIONS=("/" "/boot/efi")
  for PARTITION in "${PARTITIONS[@]}"; do
     INFO=$(df -h $PARTITION | awk 'NR==2{print $3"/"$2, $5}')
-    echo "DISK $PARTITION → $INFO"
+    log "DISK $PARTITION → $INFO"
 done
-echo "================================"
-echo "SERVICES STATUS"
-echo "================================"
+log "================================"
+log "SERVICES STATUS"
+log "================================"
 SERVICES=("docker" "ssh" "cron" "nginx" "ufw")
 for SERVICE in "${SERVICES[@]}"; do
     if systemctl is-active --quiet $SERVICE; then
-        echo "OK: $SERVICE is RUNNING"
+        log "OK: $SERVICE is RUNNING"
     else
-        echo "WARNING: $SERVICE is STOPPED"
+        log "WARNING: $SERVICE is STOPPED"
     fi
 done
 
-echo "================================"
-echo "MEMORY USAGE"
-echo "================================"
+log "================================"
+log "MEMORY USAGE"
+log "================================"
 
-free -h | awk 'NR==2{print "Total:", $2, "| Used:", $3, "| Free:", $4}'
+MEMORY=$(free -h | awk 'NR==2{print "Total:", $2, "| Used:", $3, "| Free:", $4}')
+log "$MEMORY"
